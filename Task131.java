b98742900d9eb0f4b4ddeeaaa531d48849df5a29
zdturay03 <turayzandradanes@gmail.com>
2024-07-02 05:13:28 +0000

    done adding output

---
+++ Task131.java
@@ -0,0 +1,54 @@
+package Task13;
+
+class Afritada {
+  public void showIngredients() {
+    String[] Ingredients = {"Tomato Sauce", "Meat"};
+    System.out.println("Afritada = " + java.util.Arrays.toString(Ingredients) + "🍲 \n");
+  }
+}
+
+class Mechado extends Afritada {
+  public void showIngredients() {
+    String[] Ingredients = {"Tomato Sauce", "Meat", "Potatoes & Carrots", "Bay Leaves", "Soy Sauce"};
+    System.out.println("Mechado = " + java.util.Arrays.toString(Ingredients) + "🍲 \n");
+  }
+}
+
+class Menudo extends Afritada {
+  public void showIngredients() {
+    String[] Ingredients = {"Tomato Sauce", "Meat", "Potatoes & Carrots", "Liver", "Raisins", "Hotdog"};
+    System.out.println("Menudo = " + java.util.Arrays.toString(Ingredients) + "🍲 \n");
+  }
+}
+
+class Caldereta extends Afritada {
+  public void showIngredients() {
+    String[] Ingredients = 
+      {"Tomato Sauce",
+       "Meat",
+       "Potatoes & Carrots",
+       "Tomato Paste",
+       "Liver Spread",
+       "Raisins",
+       "Hotdog",
+       "Siling Labuyo",
+       "Cheese"};
+    System.out.println("Caldereta = " + java.util.Arrays.toString(Ingredients) + "🍲 \n");
+  }
+}
+
+public class Task131 {
+    public static void main(String[] args) {
+      // add 4 meal objects here and set the type as the extended Afritada class
+        Afritada meal1 = new Afritada();
+        Afritada meal2 = new Mechado();
+        Afritada meal3 = new Menudo();
+        Afritada meal4 = new Caldereta();
+
+      // display all ingredients at every object
+        meal1.showIngredients();
+        meal2.showIngredients();
+        meal3.showIngredients();
+        meal4.showIngredients();
+    }
+}
